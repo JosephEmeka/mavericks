9bd52c9c75ef431d9ad041e8df0d070fe115bddd
JosephEmeka <josephemekaonah@gmail.com>
2024-06-12 13:45:50 +0100

    react_vite

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import 'bootstrap/dist/css/bootstrap.css'
+import Message from './Message'
+import ListGroup from './components/ListGroup'
+
+function App() {
+
+  return (
+    <>
+    <ListGroup />
+    </>
+
+  )
+}
+
+export default App
---
+++ vite-project/src/Message.tsx
@@ -0,0 +1,12 @@
+
+function Message(){
+    const name = `Joseph Emeka Onah`;
+    if (name){
+      return (<h1>{name} is Learning Vite + React</h1>)
+
+    return
+        <h1>Vite + React</h1>
+    }
+}
+
+export default Message;
